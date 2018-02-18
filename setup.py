5b4eeb961cc3a84c3ba1158e3d75639cc255c441
sina <sina@thelogically.co.uk>
2018-02-18 19:28:30 +0000

    Update version
    Signed-off-by: sina <sina@thelogically.co.uk>

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import setup
 
 setup(
     name='pynlp',
-    version='0.3.6',
+    version='0.3.7',
     packages=['pynlp'],
     install_requires=[
-        'corenlp-protobuf==3.7.1',
-        'protobuf==3.4.0',
-        'requests==2.18.4',
-        'six==1.11.0'
+        'corenlp-protobuf==3.8',
+        'protobuf==3.5.1',
+        'requests==2.18.4'
     ],
     url='http://github.com/sina-al/pynlp',
-    license='GNU General Public License, version 2',
+    license='MIT',
     author='Sina',
     author_email='s.aleyaasin@gmail.com',
     description='Python wrapper for Stanford CoreNLP'
